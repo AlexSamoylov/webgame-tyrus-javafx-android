da7420c43e6fef8a9a0ae78d443b1b0de40e0baa
oosamoylov <oosamoylov@gmail.com>
2015-12-05 21:44:17 +0200

    change port

--- common/src/main/java/org/dnu/samoylov/websocket/common/StaticData.java
+++ common/src/main/java/org/dnu/samoylov/websocket/common/StaticData.java
@@ -1,12 +1,12 @@
 package org.dnu.samoylov.websocket.common;
 
 public class StaticData {
-    public static final int SERVER_PORT = 8025;
+    public static final int SERVER_PORT = 4252;
     public static final String SERVER_CONTEXT_PATH = "/websocket";
     public static final String REGION_PATH = "/hello";
 
 
-    public static final int MAX_WAIT_SEC = 1 * 1000;
+    public static final int MAX_WAIT_SEC = 5 * 1000;
     public static final int SCORE_FOR_WIN = 20; //100
 
     public static String getServerAdress(String hostname) {
